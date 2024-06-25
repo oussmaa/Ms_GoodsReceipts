65587554ebad2ba0d95ef30edf7da1328109af08
Oussama <gharianioussama24@gmail.com>
2024-06-25 21:18:05 +0100

    last commit2

--- src/main/java/com/example/ms_goodsreceipts/Controller/StockOrderController.java
+++ src/main/java/com/example/ms_goodsreceipts/Controller/StockOrderController.java
@@ -5,10 +5,7 @@ import com.example.ms_goodsreceipts.Request.OrderStockRequest;
 import com.example.ms_goodsreceipts.service.OrderStockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orderstocks")
@@ -17,8 +14,12 @@ public class StockOrderController {
     @Autowired
     private OrderStockService orderStockService;
 
-    @PostMapping("/test")
+    @PostMapping("/addstock")
     public ResponseEntity<OrderStock> createOrderStock(@RequestBody OrderStockRequest orderStockRequest) {
         return orderStockService.createOrderStock(orderStockRequest);
+    }
+    @GetMapping
+    public Iterable<OrderStock> getAllOrderStocks() {
+        return orderStockService.getAllOrderStocks();
     }
 }
